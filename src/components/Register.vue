020d7457ff44a0fbb0a3f925a3b11118be004be6
flowerfadeaway <554222918@qq.com>
2024-07-13 20:59:30 +0800

    fix:注册提示用户已存在

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -22,6 +22,9 @@
   import { ref } from 'vue';
   import { booksManager } from '@/axios';
   import { ElMessage } from 'element-plus';
+  import { useRouter } from 'vue-router'
+
+  const router = useRouter()
   
   const registerForm = ref({
     username: '',
@@ -44,30 +47,36 @@
   
   const register = async () => {
     try {
-      // 确保密码和确认密码一致
-      if (registerForm.value.password !== registerForm.value.confirmPassword) {
-        throw new Error('密码和确认密码不匹配');
-      }
-  
-      // 确保密码长度大于等于6位
-      if (registerForm.value.password.length < 6) {
-        throw new Error('密码长度不能少于6位');
-      }
-  
-      const response = await booksManager.post('/user/register', {
-        username: registerForm.value.username,
-        password: registerForm.value.password
-      });
-  
-      console.log(response.data); // 处理响应，可能是注册成功或失败的消息
-      // 弹出注册成功提示给用户
-      ElMessage.success('注册成功！');
+        // 确保密码和确认密码一致
+        if (registerForm.value.password !== registerForm.value.confirmPassword) {
+            throw new Error('密码和确认密码不匹配');
+        }
+
+        // 确保密码长度大于等于6位
+        if (registerForm.value.password.length < 6) {
+            throw new Error('密码长度不能少于6位');
+        }
+
+        const response = await booksManager.post('/user/register', {
+            username: registerForm.value.username,
+            password: registerForm.value.password
+        });
+
+        console.log(response.data); // 处理响应，可能是注册成功或失败的消息
+        // 弹出注册成功提示给用户
+        ElMessage.success('注册成功！');
+        router.push('/login');
     } catch (error) {
-      console.error('注册失败:', error);
-      // 弹出报错提示给用户
-      ElMessage.error(error.message);
+        console.error('注册失败:', error);
+        // 弹出报错提示给用户
+        if (error.response && error.response.data && error.response.data !== '') {
+            ElMessage.error(error.response.data); // 显示服务器返回的错误消息
+        } else {
+            ElMessage.error(error.message); // 默认错误消息
+        }
     }
-  };
+};
+
   
   const validatePassword = () => {
     // 可以添加一些交互或验证逻辑，例如显示密码不匹配的消息
