53dba8f37c67521ab51124e5672cffe5e6a89726
flowerfadeaway <554222918@qq.com>
2024-07-13 12:20:58 +0800

    fix:弃用logout页面（由于目前逻辑很少）

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,37 @@
-<script setup lang="ts">
-import { computed } from 'vue'
-import { useRouter } from 'vue-router'
-import { useUserStore } from '@/stores/user'
-
-const userStore = useUserStore()
-
-// 检查用户登录状态
-userStore.checkAuth()
-
-const isLoggedIn = computed(() => userStore.isAuthenticated)
-</script>
-
 <template>
   <nav>
     <RouterLink to="/cuckoo">查询</RouterLink>
     <RouterLink to="/contact">联系我们</RouterLink>
-    <RouterLink v-if="isLoggedIn" to="/logout">登出</RouterLink>
+    <button v-if="isLoggedIn" @click="logout">登出</button>
     <RouterLink v-if="!isLoggedIn" to="/register">注册</RouterLink>
   </nav>
 
   <RouterView />
 </template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRouter, RouterLink, RouterView } from 'vue-router'
+import { useUserStore } from '@/stores/user'
+
+const router = useRouter()
+const userStore = useUserStore()
+
+// 检查用户登录状态，可以根据实际情况调整
+userStore.checkAuth()
+
+const isLoggedIn = computed(() => userStore.isAuthenticated)
+
+// 定义退出登录方法
+const logout = () => {
+  // 清除 Cookie
+  document.cookie = "AuthToken=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/"
+
+  // 清除 LocalStorage
+  localStorage.removeItem('AuthToken')
+  userStore.logout()
+
+  // 页面跳转到登录页
+  router.push({ path: '/login' }) // 确保 '/login' 是一个有效的路由路径
+}
+</script>
